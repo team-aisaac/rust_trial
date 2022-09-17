70490d130502ef3ab69e279a72eca815ca5afc75
kazurs <kwbrtpymt+git@gmail.com>
2022-09-17 12:08:52 +0900

    Protobufのシリアライズ、デシリアライズの動作確認

--- protobuf_trial/src/main.rs
+++ protobuf_trial/src/main.rs
@@ -1,3 +1,19 @@
+use protobuf::Message;
+
+mod protos;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut cmd = protos::aisaaccommand::AIsaacCommand::new();
+
+    cmd.current_x = 100;
+    cmd.current_y = 123456;
+
+    let output: Vec<u8> = cmd.write_to_bytes().unwrap();
+
+    println!("{:?}", output);
+
+    let msg = protos::aisaaccommand::AIsaacCommand::parse_from_bytes(&output).unwrap();
+    println!("cx: {}, cy: {}", msg.current_x, msg.current_y);
 }
